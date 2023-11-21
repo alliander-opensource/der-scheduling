0b3a4b1bc663ad2bc7eecd9eecea43822c0816ab
Daniel Werner <daniel.werner@ise.fraunhofer.de>
2023-11-21 13:33:33 +0100

    Test version of underlying libiec61850 version (expecting 1.5.2)

---
+++ conformance-tests/src/test/java/org/openmuc/fnn/steuerbox/VersionTest.java
@@ -0,0 +1,21 @@
+package org.openmuc.fnn.steuerbox;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class VersionTest {
+
+    public final static String EXPECTED_LIB_IEC_61850_VERSION = "1.5.2";
+
+    @Test
+    void libIecVersionMatchesExpectedVersion() throws IOException {
+        final String libIecLink = "/usr/local/lib/libiec61850.so";
+        Path path = Path.of(libIecLink).toRealPath();
+        String versionSuffix = path.toString().replaceAll(libIecLink+".","");
+
+        Assertions.assertEquals(EXPECTED_LIB_IEC_61850_VERSION,versionSuffix);
+    }
+}
